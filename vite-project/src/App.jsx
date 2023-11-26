dfa73a6c0591ea67dbc787305b161be954eb48a4
Stephany <136421961+splacito22@users.noreply.github.com>
2023-11-26 10:46:29 -0800

    imported header

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import Header from "./components/Header";
 import Navigation from "./components/Navigation";
 import Project from "./components/Project";
 import Footer from "./components/Footer";
